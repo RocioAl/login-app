88098d441f84e9624581a04f17c7e6da6f2a9b8d
RocioAl <koroinice@gmail.com>
2021-04-30 11:14:40 -0500

    add

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+const cors = require('cors');
+
+app.use(cors())
+
+
+app.get('/con-cors', cors(), (req, res, next) => {
+    res.json({ msg: 'con cors 🔝 🎉' })
+})
+
+app.get("/api", (req, res) => {
+    res.json({
+        mensaje: "Nodejs and JWT"
+    });
+});
+
+app.post("/api/login", (req, res) => {
+    const user = {
+        usuario: "Rocio",
+        password: "123"
+    }
+
+    jwt.sign({ user }, 'secretkey', { expiresIn: '32s' }, (err, token) => {
+        res.json({
+            token
+        });
+    });
+
+});
+
+app.post("/api/posts", verifyToken, (req, res) => {
+
+    jwt.verify(req.token, 'secretkey', (error, authData) => {
+        if (error) {
+            res.sendStatus(403);
+        } else {
+            res.json({
+                mensaje: "Post fue creado",
+                authData
+            });
+        }
+    });
+});
+
+// Authorization: Bearer <token>
+function verifyToken(req, res, next) {
+    const bearerHeader = req.headers['authorization'];
+
+    if (typeof bearerHeader !== 'undefined') {
+        const bearerToken = bearerHeader.split(" ")[1];
+        req.token = bearerToken;
+        next();
+    } else {
+        res.sendStatus(403);
+    }
+}
+
+app.listen(3000, () => {
+    console.log("nodejs app running...");
+});
